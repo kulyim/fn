c728b17935c6ac690530ff3846af602712765acf
kulyim <ysashi@gmail.com>
2018-03-22 15:07:22 -0400

    remove go.sh

--- go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PU='vendor/bin/phpunit'
-AUTO='--bootstrap vendor/autoload.php'
-CMD="$PU $AUTO ${1+"$@"} $2"
-if [ "$#" -ne "1" ]; then
-    echo "Usage $0 testfoldername"
-    exit
-fi
-$CMD
